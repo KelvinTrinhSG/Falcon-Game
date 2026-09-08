c6efaacdc0f7adef048bff48a0a5ef1bea09e5c7
KelvinTrinhSG <126239251+KelvinTrinhSG@users.noreply.github.com>
2026-09-08 14:32:12 +0700

    Revert "feat: turret hoàn trả inventory khi xóa, block xóa vĩnh viễn"
    This reverts commit c3cc9523080be4790f62a70df051595b2a0c672c.

--- src/ServerScriptService/Controllers/PlacementController.lua
+++ src/ServerScriptService/Controllers/PlacementController.lua
@@ -303,7 +303,6 @@ local function onRemoveItem(player: Player, itemToRemove: Model)
 
 	if config.Type == "Turrets" then
 		TurretController:RemoveTurret(itemToRemove)
-		profile.Data.BlockInventory[itemId] = (profile.Data.BlockInventory[itemId] or 0) + 1
 	end
 
 	itemToRemove:Destroy()
